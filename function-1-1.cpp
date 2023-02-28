9e9e92896b7ffcf9db15d1afbcff263064cb5817
Deathmatter15 <deathmatter@ChuVetruvian.localdomain>
2023-03-01 00:29:58 +1030

    Formatting changes in function-1-1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,20 +2,20 @@
 //function that returns sum of all elements in a array
 //Input is the int array and the number of elements
 //Return 0 if n < 1
-int sumArrayElements( int array[] , int n){
-    int elementSum = 0;
+int array_sum (int array[] , int n){
+    int array_sum = 0;
     
     if (n < 1){
-        //Keep elementSum = 0
+        //Keep array_sum = 0
 
         else if (n >= 1){
 
-            for(int i = 0 ; i < n ; i++){
-                elementSum += array[i]
+            for (int i = 0 ; i < n ; i++){
+                array_sum += array[i];
 
             }
         }
     }
 
-    return elementSum // return "z" 
+    return array_sum // return "z" 
 }
