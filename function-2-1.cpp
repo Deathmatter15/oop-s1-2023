b47127aae92f44383689125e8bdf85d7bc7926aa
Deathmatter15 <deathmatter@ChuVetruvian.localdomain>
2023-03-01 01:00:38 +1030

    completetion of function-2-1, function-2-2, and progress in function-2-3

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -4,7 +4,7 @@ int min_element (int array[], int n){
     int array_minimum = 0;
 
     if (n < 1) {
-        //retain array_minimum = o if n < 1 
+        //retain array_minimum = 0 if n < 1 
 
         else if(n >= 1){
             array_minimum = array[0];
---
+++ function-2-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+
+int max_element (int array[], int n){
+    int array_maximum = 0;
+
+    if (n < 1) {
+        //retain array_maximum = 0 if n < 1 
+
+        else if(n >= 1){
+         array_maximum = array[0];
+            
+            for(i=1; i<n; i++){
+                if (array_maximum < array[i]){
+                    int array_maximum = array[i]
+                }
+
+            }
+
+        }
+    }
+return array_maximum
+}
