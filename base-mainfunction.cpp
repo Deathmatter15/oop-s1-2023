cc3bc3765be28c84022f4d1ce6f929e6c5884cb6
Deathmatter15 <deathmatter@ChuVetruvian.localdomain>
2023-02-28 22:02:27 +1030

    finish main and function script

---
+++ base-mainfunction.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+void functionName( "v-Type" x , "v-Type" y){ // "v-Type" if there is an output. 
+
+    //Body
+
+return 0 // return "v-Type" z 
+}
+
+int main(){
+
+    //Body
+
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-
-// The `extern` keyword tells the compiler that the count function exists,
-// but will be implemented somewhere else
-extern int count(int[], int);
 
 int main() {
     int array[5] = {4,5,6,7,8};
