e48e7abef1d7e0d1ba42aae957d477bbbf5410d7
Deathmatter15 <deathmatter@ChuVetruvian.localdomain>
2023-03-02 11:43:18 +1030

    files v1 complete, bugfix needed for 3-1

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -1,14 +1,17 @@
-#include 
+#include <iostream>
 
 double sum_even(double array[], int n){
     double array_sum_evenPosition = 0;
     if (n<1){
         //retain array_sum_evenPosition = 0
-        else{
-            for(i=0; i<n: i+=2){
-                double array_sum_evenPosition = array[i]
-            }
+    }
+    
+    else{
+
+        for (int i=0; i<n; i+=2){
+            array_sum_evenPosition += array[i];
         }
     }
+    
     return array_sum_evenPosition;
 }
--- main-3-5.cpp
+++ main-3-5.cpp
@@ -1,7 +1,13 @@
 #include <iostream> 
 
+extern double sum_even(double array[], int n);
+
 int main(){
+    double array_int[5] = {3,2,5,4,6};
+    double array_floating[5] = {1.2, 2.8, 3.8, 1.4, 1.9};
 
-    //Body
+    std::cout << sum_even(array_int, 5) << "\n";
+    std::cout << sum_even(array_floating, 5) << "\n";
 
+    return 0;
 }
