50bf37d58238f181f8314c9acac90dce13697a30
Deathmatter15 <deathmatter@ChuVetruvian>
2023-03-07 20:58:03 +1030

    Remove unneeded files

--- main-1-5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-extern void print_summed(int array1[3][3],int array2[3][3]);
-
-int main(){
-    int array_first[3][3] = {{2,2,2}, {2,2,2}, {2,2,2}}, array_second[3][3] ={{1,2,3}, {4,5,6}, {7,8,9}};
-    print_summed(array_first,array_second);
-    
-return 0;
-}
