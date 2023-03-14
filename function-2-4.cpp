adde37a8dfc7961c71308c0abdda3536613cfe44
Deathmatter15 <deathmatter@ChuVetruvian>
2023-03-15 01:28:58 +1030

    added + & - operations

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -4,6 +4,17 @@ float multiply_op(float left, float right) {
     float tim_float = left*right; 
     return tim_float;
 }
+
+float add_op (float left, float right){
+    float sum_float = left + right; 
+    return sum_float;
+}
+
+float subtract_op (float left, float right){
+    float sub_float = left - right; 
+    return sub_float;
+}
+
 float arithmetic_ops(float left, float right, float (*op)(float,float))
 {
 	return (*op)(left, right);
