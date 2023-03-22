ffdedef9529741fa46951964dab8cb044c8d3906
Deathmatter15 <deathmatter@ChuVetruvian>
2023-03-22 13:17:25 +1030

    change 3.2 equalsArray to reverseArray

--- main-3-2.cpp
+++ main-3-2.cpp
@@ -1,13 +1,13 @@
 #include <iostream> 
 extern int *readNumbers();
 extern void printNumbers(int *numbers, int length);
-extern int* equalsArray(int *numbers, int length);
+extern int* reverseArray(int *numbers, int length);
 using namespace std; 
 
 int main() { 
     int *numbers = readNumbers(); 
     cout << endl;
-    int* numbers1 = equalsArray(numbers, 10);
+    int* numbers1 = reverseArray(numbers, 10);
     
     for (int i = 0; i < 10; i++){
         cout << numbers1[i] << endl;
