3e58bcf88bd8ef5c0476ea35c5f5e318e2c775af
Deathmatter15 <deathmatter@ChuVetruvian>
2023-03-22 11:10:37 +1030

    changing letter cases

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -22,20 +22,45 @@ void hexDigits(int *numbers, int length){
         std::cout << i << " " << numbers[i] << " "; 
 
         switch(numbers[i]) {
-            case 0: std::cout << '0'; break;
-            case 1: std::cout << '1'; break;
-            case 2: std::cout << '2'; break;
-            case 3: std::cout << '3'; break;
-            case 4: std::cout << '4'; break;
-            case 5: std::cout << '5'; break;
-            case 6: std::cout << '6'; break;
-            case 7: std::cout << '7'; break;
-            case 8: std::cout<<'8';
-                break;
+            case 0: 
+                std::cout << 0 << std::endl;
+            break;
 
-            case 9:
-                std::cout<<'9';
-                break;
+            case 1: 
+                std::cout << 1 << std::endl;
+            break; 
+
+            case 2: 
+                std::cout << 2 << std::endl;
+            break; 
+
+            case 3: 
+                std::cout << 3 << std::endl;
+            break; 
+
+            case 4: 
+                std::cout << 4 << std::endl;
+            break; 
+
+            case 5: 
+                std::cout << 5 << std::endl;
+            break; 
+
+            case 6: 
+                std::cout << 6 << std::endl;
+            break; 
+
+            case 7: 
+                std::cout << 7 << std::endl;
+            break;
+
+            case 8: 
+                std::cout << 8 << std::endl;
+            break;
+
+            case 9: 
+                std::cout << 9 << std::endl;
+            break;
 
             default:
                 char c='A'+ (numbers[i]-10);
