90f348b27095e674b6fb022f99fdc777407999b9
Deathmatter15 <deathmatter@ChuVetruvian>
2023-03-23 12:47:17 +1030

    1-2 is just 1-3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,7 +1,13 @@
- #include <iostream> 
-#include ""
+#include <iostream> 
+#include "Person.h"
 
-void name(){
-
-    return;
+PersonList createPersonList(int n){
+    PersonList Book; 
+    Book.people = new Person[n];
+    for (int i=0; i<n; i++) {
+        Book.people[i].name = "Jane Doe";
+        Book.people[i].age = 1;
+        Book.numPeople = n;  
+    }
+    return Book;
 }
