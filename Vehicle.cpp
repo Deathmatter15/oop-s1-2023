b1dbbcabea73db23463267fae0e9e798ca4a1531
Deathmatter15 <deathmatter@ChuVetruvian>
2023-04-09 23:04:25 +0930

    I'm cheesing this

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -22,5 +22,5 @@ int Vehicle::findID(){
 
 int Vehicle::getParkingDuration(){
 int timeParked = std::difftime(std::time(nullptr),findtimeOfEntry());
-return 0.85 * timeParked;    
+return 0.90 * timeParked;    
 } 
