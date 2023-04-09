edadc513f2bd72a12353b9478cdfe54bdf65c707
Deathmatter15 <deathmatter@ChuVetruvian>
2023-04-09 21:30:56 +0930

    Added Case Sensitive File

---
+++ Vehicle.h
@@ -0,0 +1,24 @@
+#ifndef VehicleFiveOFour 
+#define VehicleFiveOFour
+#include <string.h> 
+#include <ctime>
+
+class Vehicle {
+    
+    private:
+
+    std::time_t timeOfEntry;
+    int ID; 
+
+    public:
+    Vehicle();
+
+    Vehicle(int given_ID);
+
+    std::time_t findtimeOfEntry();
+    
+    
+    int findID();
+
+};
+#endif
