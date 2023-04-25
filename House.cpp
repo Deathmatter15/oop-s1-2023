6868bf640d60ac0e33c8f5d680a7dc3b472bfc65
Deathmatter15 <deathmatter@ChuVetruvian>
2023-04-25 18:25:57 +0930

    Solved Segmentation fault do to bad initialisation of array

--- House.cpp
+++ House.cpp
@@ -3,22 +3,24 @@
 
 House::House(){
     numAppliances = 0;
-    *appliances = new Appliance[0];
+    appliances = new Appliance*[0];
 }
 
 House::House(int input_numAppliances) {
     numAppliances = 0;
     numAppliances_max = input_numAppliances;
-    *appliances = new Appliance[input_numAppliances];
+    appliances = new Appliance*[input_numAppliances];
 }
 
 bool House::addAppliance(Appliance* appliance) {
     if(numAppliances < numAppliances_max){
-    appliances[numAppliances] = appliance;
-    numAppliances++;
-    return true;
+        appliances[numAppliances] = appliance;
+        numAppliances++;
+        return true;
+    } else if (numAppliances >= numAppliances_max){
+        return false;
     } else {
-    return false;
+        return false;
     }
 }
 
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -1,6 +1,18 @@
 #include <iostream> 
+#include "Appliance.h"
+#include "Fridge.h"
+#include "TV.h"
+#include "House.h"
 using namespace std;
 
 int main(){
+Fridge *Coppenhein = new Fridge(5,200);
+TV *Benq = new TV(5, 200);
+Appliance *Base = new Appliance(2);
 
+House *Hans = new House(3);
+cout << "Checkpoint 1" << endl;
+cout << Hans->addAppliance(Coppenhein) << endl;
+cout << Hans->addAppliance(Benq) << endl;
+cout << Hans->addAppliance(Base) << endl;
 }
