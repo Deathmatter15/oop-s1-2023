655a3697f04d7b041325968da55729d1b768d072
Deathmatter15 <deathmatter@ChuVetruvian>
2023-04-27 13:58:18 +0930

    Bazinga

--- Fleet.h
+++ Fleet.h
@@ -1,4 +1,13 @@
-#ifndef someNameHere
-#define someNameHere
-    //Body
+#ifndef FleetTwentySeven
+#define FleetTwentySeven
+    #include "Car.h"
+    class Fleet {
+        private:
+        Car** fleet; 
+
+        public:
+        Fleet();
+
+        Car **get_fleet();
+    };
 #endif
