f4d114472fb2b58b06ace9927dbc4acb8a0994a1
Deathmatter15 <hans_chu@outlook.com>
2023-05-24 13:39:23 +0930

    Lmao?

---
+++ ws11.cpp
@@ -0,0 +1,20 @@
+#include "RecordsManagers.h"
+
+int main() {
+        
+        RecordsManager receordM("test_clean.txt");
+        // RecordsManager receordM("test_corrupt1.txt");
+        // RecordsManager receordM("test_corrupt2.txt");
+        Records myRecords;
+
+        // reads records
+        receordM.read(myRecords);
+
+        // calculate and print out the sum
+        int sum = 0;
+        for (int i = 0; i < myRecords.size(); i++) {
+             sum += myRecords[i];
+        }
+        cout << sum << endl;
+    return 0;
+}
