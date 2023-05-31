03abc0b95be4b23221acfeb5c9bef157c9180d9e
Deathmatter15 <deathmatter@ChuVetruvian>
2023-05-31 14:49:57 +0930

    Definition in Utils Header

--- Utils.h
+++ Utils.h
@@ -9,7 +9,15 @@ class Utils {
     private:
 
     public:
-    std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight);
-    double calculateDistance(std::tuple<int,int> pos1, std::tuple<int,int> pos2);
+    std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight){
+    int x = rand() % (gridWidth + 1);
+    int y = rand() % (gridHeight + 1);
+    std::tuple<int, int> new_pos(x, y);
+    return new_pos;
+    };
+    double Utils::calculateDistance(std::tuple<int,int> pos1, std::tuple<int,int> pos2){
+    int distance_squared = pow(std::get<0>(pos1)+std::get<0>(pos2), 2) + pow(std::get<1>(pos1) + std::get<1>(pos2), 2);
+    return pow(distance_squared, 0.5);
+    };
 };
 #endif
